2af6e06e545b9a8eaf5381ef85ba0f972c41176e
Brayden Kim <dudgjs1318@gmail.com>
2022-12-05 15:41:54 -0800

    quick fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,8 +18,8 @@ namespace PetSitter.Controllers
 
         public IActionResult Index()
         {
-            IQueryable<Pet> pets = from p in _db.Pets
-                                   select p;
+            IQueryable<Pet> pets = from pts in _db.Pets
+                                   select pts;
             return View(pets);
         }
 
