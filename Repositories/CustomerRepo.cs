e7078bb0ab344a73a8cfd02f16c0abbe908c5cb7
Brayden Kim <dudgjs1318@gmail.com>
2022-12-07 15:32:40 -0800

    register part is done!

---
+++ Repositories/CustomerRepo.cs
@@ -0,0 +1,21 @@
+﻿using PetSitter.Data;
+using PetSitter.Models;
+
+namespace PetSitter.Repositories
+{
+    public class CustomerRepo
+    {
+        PetSitterContext _db;
+
+        public CustomerRepo(PetSitterContext context)
+        {
+            _db = context;
+        }
+
+        public void AddUser(User user)
+        {
+            _db.Users.Add(user);
+            _db.SaveChanges();
+        }
+    }
+}
