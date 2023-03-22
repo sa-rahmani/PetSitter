a56b2967194a29f682ffe98a79b5ad7d8f8853b3
Sam VH <svanharen@my.bcit.ca>
2023-03-22 13:30:52 -0700

    Admin components added, set up a view all users table in AdminDashboard on nav bar

---
+++ Repositories/AdminRepo.cs
@@ -0,0 +1,34 @@
+﻿using PetSitter.Models;
+using PetSitter.ViewModels;
+
+namespace PetSitter.Repositories
+{
+    public class AdminRepo
+    {
+        private readonly PetSitterContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AdminRepo(PetSitterContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IQueryable GetAllUsers() 
+        {
+          var allUsers = (from user in _db.Users
+                            select new AdminDashboardVM
+                            {
+                                UserID = user.UserId,
+                                Email = user.Email,
+                                FirstName = user.FirstName,
+                                LastName = user.LastName,
+                                Role = user.UserType
+                            });
+
+            return allUsers;
+
+           
+        }
+    }
+}
