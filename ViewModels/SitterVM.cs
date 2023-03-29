ff922f393ae410e99a0fb95118190d12257449f0
Lauren <lbyrne7@my.bcit.ca>
2023-03-29 10:30:18 -0700

    tidied code for view models

--- ViewModels/SitterVM.cs
+++ ViewModels/SitterVM.cs
@@ -19,13 +19,8 @@ namespace PetSitter.ViewModels
 
         [DisplayName("Average Rating")]
         public double? AvgRating { get; set; }
-
         public List<ReviewVM>? Reviews { get; set; }
-
-        //public List<string>? Reviews { get; set; }
-
         public List<string>? petTypes { get; set; }
-
         public List<DateTime>? availableDates { get; set; }
         public List<Availability>? availabilities  { get; set; }
     }
