22e938f74f049c41c80ea6d5fdbff108eb3a0ed7
Sam VH <svanharen@my.bcit.ca>
2023-04-10 15:04:36 -0700

    admin dashboard and userroles combined

--- Migrations/20230410201840_adminIssues.Designer.cs
+++ Migrations/20230410201840_adminIssues.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetSitter.Models;
 
@@ -11,9 +12,10 @@ using PetSitter.Models;
 namespace PetSitter.Migrations
 {
     [DbContext(typeof(PetSitterContext))]
-    partial class PetSitterContextModelSnapshot : ModelSnapshot
+    [Migration("20230410201840_adminIssues")]
+    partial class adminIssues
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
